cb15301a10e09926717cea4e7615faee6a87d300
laribi houssem <laribihoussem1998@gmail.com>
2022-01-29 16:10:20 +0100

    first commit

---
+++ demo/demo/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+
+}
